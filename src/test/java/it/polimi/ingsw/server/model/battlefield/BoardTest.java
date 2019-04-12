cf5e7d08e7572063ccd3e60beca41ec5dca5bb0f
Riccardo Colella <riccardo.colella@mail.polimi.it>
2019-04-12 09:07:20 +0200

    Test-Model - Added introductory comments explaining BoardTest methods - Fixed few variable names according to the file agreement. Now they all should be universal

--- src/test/java/it/polimi/ingsw/server/model/battlefield/BoardTest.java
+++ src/test/java/it/polimi/ingsw/server/model/battlefield/BoardTest.java
@@ -32,6 +32,9 @@ class BoardTest {
     private List<PlayerInfo> playerInfos = new LinkedList<>();
     private List<Player> players = new LinkedList<>();
 
+    /**
+     * Creating boards and player to be used for tests
+     */
     @BeforeEach
     void setUp() {
         //Creating boards
@@ -47,21 +50,36 @@ class BoardTest {
 
     }
 
+    /**
+     * This test covers the method getBlock() in the following situations:
+     * - getting a block non present in the board
+     * - getting a block in row -1, in position column.size e row.size (out of bounds)
+     * - getting some block that should exist
+     */
     @Test
     void getBlock() {
         //Testing exceptions
         //Trying to select a non existing block
         assertFalse(board2.getBlock(2,0).isPresent(), "1. Error with a non present block in BOARD_2");
+
         //Testing bounds
         assertFalse(board2.getBlock(-1, 2).isPresent(), "2. Error in bound exception.");
         assertFalse(board2.getBlock(0,board2.getColumnLenght()).isPresent(), "3a. Error in bound exception.");
         assertFalse(board2.getBlock(board2.getRowLenght(), 0).isPresent(), "3b. Error in bound exception.");
+
         //Trying to select some existing blocks
         assertTrue(board2.getBlock(1,1).isPresent(), "4. Error in picking block in BOARD_2.");
         assertTrue(board3.getBlock(1,1).isPresent(), "5. Error in picking block in BOARD_3.");
 
     }
 
+    /**
+     * This test covers the method getBlockNeighbor() in the following situations:
+     * - getting a block out of bounds
+     * - getting an existing block
+     * - getting a non existing block in the board
+     * - getting some others existing blocks and out of bounds blocks on another board
+     */
     @Test
     void getBlockNeighbor() {
         //Tests on BOARD_1
@@ -87,16 +105,20 @@ class BoardTest {
         Optional<Block> block_4_0_2 = board4.getBlock(0,2);
         Optional<Block> block_4_0_3 = board4.getBlock(0,3);
         Optional<Block> block_4_1_0 = board4.getBlock(1,0);
-
         //Trying to get an existing block
         block_4_0_2.ifPresent(block -> assertEquals(block_4_0_3, board4.getBlockNeighbor(block, Direction.EAST), "5. Error in EAST direction picking with BOARD_4."));
-        //Trying to get an existing
+        //Trying to get an existing block
         block_4_0_0.ifPresent(block -> assertEquals(block_4_1_0, board4.getBlockNeighbor(block, Direction.SOUTH), "6. Error in SOUTH direction picking with BOARD_4."));
         //Trying to get an out-of-bounds block
         block_4_0_0.ifPresent(block -> assertFalse(board4.getBlockNeighbor(block, Direction.NORTH).isPresent(), "7. Error in bound exception."));
         block_4_0_0.ifPresent(block -> assertFalse(board4.getBlockNeighbor(block, Direction.WEST).isPresent(), "8. Error in bound exception."));
     }
 
+    /**
+     * This test covers the method getVisibleBlocks() in the following situations:
+     * - getting a complex visible blocks's list
+     * - getting a another complex visible blocks's list
+     */
     @Test
     void getVisibleBlocks() {
         //Test on BOARD_4
@@ -139,6 +161,12 @@ class BoardTest {
         assertEquals(asserted, board1.getVisibleBlocks(block_1_1_2.get()));
     }
 
+    /**
+     * This test covers the method getRoom() in the following situations:
+     * - getting a room from different starting blocks passed as parameter
+     * - getting a single-block room
+     * - getting a complex room
+     */
     @Test
     void getRoom() {
         //Tests on BOARD_1
@@ -146,23 +174,23 @@ class BoardTest {
         Optional<Block> block_1_2_1 = board1.getBlock(2,1);
         Optional<Block> block_1_2_2 = board1.getBlock(2,2);
         // Creating Set variable that will contain the expected result
-        Set<Block> roomWhite10 = new HashSet<>();
+        Set<Block> roomWhite1 = new HashSet<>();
         //Building the expected result
-        block_1_2_1.ifPresent(roomWhite10::add);
-        block_1_2_2.ifPresent(roomWhite10::add);
+        block_1_2_1.ifPresent(roomWhite1::add);
+        block_1_2_2.ifPresent(roomWhite1::add);
         //trying to select a room of 2 blocks passing one block
-        block_1_2_2.ifPresent(block -> assertEquals(roomWhite10, board1.getRoom(Objects.requireNonNull(block)), "1. Error in getting white room in BOARD 10 part1of2."));
+        block_1_2_2.ifPresent(block -> assertEquals(roomWhite1, board1.getRoom(Objects.requireNonNull(block)), "1. Error in getting white room in BOARD 1 part1of2."));
         //trying to select the room of 2 blocks passing the other block
-        block_1_2_1.ifPresent(block -> assertEquals(roomWhite10, board1.getRoom(block), "2. Error in getting white room in BOARD 10 part2of2."));
+        block_1_2_1.ifPresent(block -> assertEquals(roomWhite1, board1.getRoom(block), "2. Error in getting white room in BOARD 1 part2of2."));
 
         //Tests on BOARD_2
         //Getting some Optional<block> containing blocks expected as result
         Optional<Block> block_2_0_3 = board2.getBlock(0,3);
         // Creating Set variable that will contain the expected result
-        Set<Block> roomGreen11a = new HashSet<>();
+        Set<Block> roomGreen2 = new HashSet<>();
         //Testing a single-block room
-        block_2_0_3.ifPresent(roomGreen11a::add);
-        block_2_0_3.ifPresent(block -> assertEquals(roomGreen11a, board2.getRoom(block), "3. Error in getting green room in BOARD_2."));
+        block_2_0_3.ifPresent(roomGreen2::add);
+        block_2_0_3.ifPresent(block -> assertEquals(roomGreen2, board2.getRoom(block), "3. Error in getting green room in BOARD_2."));
 
         //Getting some Optional<block> containing blocks expected as result
         Optional<Block> block_2_1_2 = board2.getBlock(1,2);
@@ -170,16 +198,21 @@ class BoardTest {
         Optional<Block> block_2_2_2 = board2.getBlock(2,2);
         Optional<Block> block_2_2_3 = board2.getBlock(2,3);
         // Creating Set variable that will contain the expected result
-        Set<Block> roomYellow11a = new HashSet<>();
+        Set<Block> roomYellow2 = new HashSet<>();
         //Adding blocks to the expected result
-        block_2_1_2.ifPresent(roomYellow11a::add);
-        block_2_1_3.ifPresent(roomYellow11a::add);
-        block_2_2_2.ifPresent(roomYellow11a::add);
-        block_2_2_3.ifPresent(roomYellow11a::add);
+        block_2_1_2.ifPresent(roomYellow2::add);
+        block_2_1_3.ifPresent(roomYellow2::add);
+        block_2_2_2.ifPresent(roomYellow2::add);
+        block_2_2_3.ifPresent(roomYellow2::add);
         //Testing a more complex room
-        block_2_1_2.ifPresent(block -> assertEquals(roomYellow11a, board2.getRoom(block), "4. Error in getting yellow room in BOARD_2."));
+        block_2_1_2.ifPresent(block -> assertEquals(roomYellow2, board2.getRoom(block), "4. Error in getting yellow room in BOARD_2."));
     }
 
+    /**
+     * This test covers the method getRow() in the following situations:
+     * - getting a row with a null block in the board
+     * - getting a full row
+     */
     @Test
     void getRow() {
         //Getting some Optional<block> containing blocks expected as result
@@ -211,6 +244,11 @@ class BoardTest {
         block_1_1_2.ifPresent(block -> assertEquals(row1InBoard1, board1.getRow(block)));
     }
 
+    /**
+     * This test covers the method getColumn() in the following situations:
+     * - getting a column with a null block in the board
+     * - getting a full column
+     */
     @Test
     void getColumn() {
         //Testing using board1
@@ -239,6 +277,13 @@ class BoardTest {
         block_1_2_1.ifPresent(block -> assertEquals(column1InBoard1, board1.getColumn(block)));
     }
 
+    /**
+     * This test covers the method movePlayer() in the following situations:
+     * - moving player with an invalid move (destination out-of-bounds)
+     * - moving player with some valid moves
+     * - moving player with many players on the board
+     * - moving player who is present in more than one position in the board
+     */
     @Test
     void movePlayer() {
         //Testing null block on BOARD_1
@@ -282,7 +327,7 @@ class BoardTest {
         //Removing player from block
         block_1_2_3.ifPresent(block -> block.removePlayer(player0));
 
-        //Testing multiple players on BOARD_10
+        //Testing multiple players on BOARD_1
         //Checking corrected setup
         block_1_2_3.ifPresent(block -> assertEquals(0,block.getPlayers().size()));
         //Adding many players to a block
@@ -313,6 +358,13 @@ class BoardTest {
         block_1_2_3.ifPresent(block -> assertEquals(0, block.getPlayers().size()));
     }
 
+    /**
+     * This test covers the method findPlayer() in the following situations:
+     * - finding a single player on the board
+     * - finding a player not alone in the board
+     * - finding a player not alone on his block
+     * - finding a player who is present in more than one position (IllegalStateException)
+     */
     @Test
     void findPlayer() {
         int i = 4;
@@ -355,6 +407,12 @@ class BoardTest {
         block_4_2_2.ifPresent(block -> block.removePlayer(playerUbiquitous));
     }
 
+    /**
+     * This test covers the method teleportPlayer() in the following situations:
+     * - teleporting a single player on the board
+     * - teleporting a player not alone on the board
+     * - teleporting a player not present on the board
+     */
     @Test
     void teleportPlayer() {
         int i = 2;
@@ -362,29 +420,29 @@ class BoardTest {
         int j = 0;
         //Trying to teleport a single player on the board
         //Getting some Optional<block> containing blocks expected as result
-        Optional<Block> block_12_2_2 = board4.getBlock(2,2);
-        Optional<Block> block_12_0_0 = board4.getBlock(0,0);
+        Optional<Block> block_6_2_2 = board4.getBlock(2,2);
+        Optional<Block> block_4_0_0 = board4.getBlock(0,0);
         //Adding the player to the board
-        block_12_2_2.ifPresent(block -> block.addPlayer(players.get(i)));
+        block_6_2_2.ifPresent(block -> block.addPlayer(players.get(i)));
         //Executing teleportPlayer
-        block_12_0_0.ifPresent(destinationBlock -> board4.teleportPlayer(players.get(i), destinationBlock));
+        block_4_0_0.ifPresent(destinationBlock -> board4.teleportPlayer(players.get(i), destinationBlock));
         //Checking result
-        assertEquals(block_12_0_0, board4.findPlayer(players.get(i)));
+        assertEquals(block_4_0_0, board4.findPlayer(players.get(i)));
 
         //Trying to teleport a player not alone in the board
         //Adding another player to the board
-        block_12_2_2.ifPresent(block -> block.addPlayer(players.get(k)));
+        block_6_2_2.ifPresent(block -> block.addPlayer(players.get(k)));
         //Executing teleportPlayer
-        block_12_2_2.ifPresent(destinationBlock -> board4.teleportPlayer(players.get(i), destinationBlock));
+        block_6_2_2.ifPresent(destinationBlock -> board4.teleportPlayer(players.get(i), destinationBlock));
         //Checking result
-        assertEquals(block_12_2_2, board4.findPlayer(players.get(i)));
-        assertEquals(block_12_2_2, board4.findPlayer(players.get(k)));
+        assertEquals(block_6_2_2, board4.findPlayer(players.get(i)));
+        assertEquals(block_6_2_2, board4.findPlayer(players.get(k)));
 
         //testing illegal state (player non present on board)
-        assertTrue(block_12_0_0.isPresent());
-        assertThrows(NullPointerException.class, () -> board4.teleportPlayer(players.get(j), block_12_0_0.get()));
+        assertTrue(block_4_0_0.isPresent());
+        assertThrows(NullPointerException.class, () -> board4.teleportPlayer(players.get(j), block_4_0_0.get()));
         //Cleaning scenario
-        block_12_2_2.ifPresent(block -> block.removePlayer(players.get(i)));
-        block_12_2_2.ifPresent(block -> block.removePlayer(players.get(k)));
+        block_6_2_2.ifPresent(block -> block.removePlayer(players.get(i)));
+        block_6_2_2.ifPresent(block -> block.removePlayer(players.get(k)));
     }
 }
